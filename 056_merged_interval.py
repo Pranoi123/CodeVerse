91381d585acfa2b04b782297a2f3576e8474efb3
PranavT <pranavtbuild@gmail.com>
2025-04-14 11:32:10 +0530

    56. Merge Intervals done

---
+++ 056_merged_interval.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def merge(self, intervals):
+        if not intervals:
+            return []
+        intervals.sort(key=lambda x: x[0])
+        result = [intervals[0]]
+        for current in intervals[1:]:
+            last_merged = result[-1]
+            if current[0] <= last_merged[1]:  
+                last_merged[1] = max(last_merged[1], current[1])
+            else:
+                result.append(current)
+        
+        return result
