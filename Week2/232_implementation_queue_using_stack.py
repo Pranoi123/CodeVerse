ac5cb7fa9983c585d619438dd9c2d3bd1470b7d3
Pranoi123 <pranavtalwalkarbech24@rvu.edu.in>
2025-04-24 22:13:20 +0530

    Queues part updated

---
+++ Week2/232_implementation_queue_using_stack.py
@@ -0,0 +1,30 @@
+class MyQueue:
+    def __init__(self):
+        self.stk1 = []
+        self.stk2 = [] # reversed order
+
+    def push(self, x: int) -> None:
+
+        self.stk1.append(x)
+
+    def pop(self) -> int:
+        self.move()
+        return self.stk2.pop()
+
+    def peek(self) -> int:
+        self.move()
+        return self.stk2[-1]
+
+    def empty(self) -> bool:
+        return not self.stk1 and not self.stk2
+
+    def move(self):
+        if not self.stk2: # only when skt2 is empty
+            while self.stk1:
+                self.stk2.append(self.stk1.pop())
+# Your MyQueue object will be instantiated and called as such:
+# obj = MyQueue()
+# obj.push(x)
+# param_2 = obj.pop()
+# param_3 = obj.peek()
+# param_4 = obj.empty()
